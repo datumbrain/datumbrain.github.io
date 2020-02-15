e08431245619603c05f354e21ce492a76bacf387
Shahid Ali <shahid@datumbrain.com>
2020-02-15 14:33:32 +0500

    [dbd-78] Website Changes - Incorporates icons in bubble section - Add carousel for featured projects - Add Upwork Icon and give link https://www.upwork.com/ag/datumbrain/

--- src/components/Navbars/DefaultNavbar.jsx
+++ src/components/Navbars/DefaultNavbar.jsx
@@ -6,123 +6,130 @@ import classNames from "classnames/bind";
 import { NavbarBrand, NavItem, NavLink, Nav, Container } from "reactstrap";
 
 class DefaultNavbar extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      navbarExpanded: false
+        this.state = {
+            navbarExpanded: false
+        };
+
+        this.navbar = React.createRef();
+
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+    }
+
+    toggleNavbar() {
+        this.setState(prevState => ({
+            navbarExpanded: !prevState.navbarExpanded
+        }));
+    }
+
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
+
+    handleScroll = () => {
+        const navigation = this.navbar.current;
+        const navTop = navigation.offsetTop + navigation.offsetHeight;
+
+        if (window.scrollY >= navTop) {
+            navigation.classList.add("navbar-sticky");
+        } else {
+            navigation.classList.remove("navbar-sticky");
+        }
     };
 
-    this.navbar = React.createRef();
+    render() {
+        const useOnlyDarkLogo = this.props.useOnlyDarkLogo;
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-  }
+        return (
+            <nav
+                ref={this.navbar}
+                className={classNames(
+                    "navbar navbar-expand-md main-nav navigation fixed-top sidebar-left",
+                    { "navbar-expanded": this.state.navbarExpanded }
+                )}
+            >
+                <Container>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        onClick={this.toggleNavbar}
+                    >
+                        <span className="icon-bar" />
+                        <span className="icon-bar" />
+                        <span className="icon-bar" />
+                    </button>
 
-  toggleNavbar() {
-    this.setState(prevState => ({
-      navbarExpanded: !prevState.navbarExpanded
-    }));
-  }
+                    <NavbarBrand className="mr-lg-5" to="/" tag={Link}>
+                        {useOnlyDarkLogo ? (
+                            <img
+                                alt="..."
+                                src={require("assets/img/logo.png")}
+                                className="logo logo-sticky"
+                            />
+                        ) : (
+                            <>
+                                <img
+                                    src={require("assets/img/logo.png")}
+                                    alt="Laapp"
+                                    className="logo logo-sticky d-block d-md-none"
+                                />
+                                <img
+                                    src={require("assets/img/logo-light.png")}
+                                    alt="Laapp"
+                                    className="logo d-none d-md-block"
+                                />
+                            </>
+                        )}
+                    </NavbarBrand>
 
-  componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll);
-  }
+                    <div className="collapse navbar-collapse">
+                        <div className="sidebar-brand">
+                            <Link to="/">
+                                <img
+                                    src={require("assets/img/logo.png")}
+                                    alt="Laapp Template"
+                                    className="logo"
+                                />
+                            </Link>
+                        </div>
 
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.handleScroll);
-  }
-
-  handleScroll = () => {
-    const navigation = this.navbar.current;
-    const navTop = navigation.offsetTop + navigation.offsetHeight;
-
-    if (window.scrollY >= navTop) {
-      navigation.classList.add("navbar-sticky");
-    } else {
-      navigation.classList.remove("navbar-sticky");
+                        <Nav className="nav navbar-nav ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="#home">Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#features">About</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#portfolio">Portfolio</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="http://blog.datumbrain.com" target="_blank">
+                                    Blog
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#contact">Contact</NavLink>
+                            </NavItem>
+                            <NavItem className="d-flex align-items-center">
+                                <img src={require("../../assets/img/upwork_icon.png")} style={{
+                                    height: "30px",
+                                    transform: "translateX(10px)"
+                                }} alt=""/>
+                                <NavLink target={"blank"} href="https://www.upwork.com/ag/datumbrain/" style={{}}>Upwork</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </div>
+                </Container>
+            </nav>
+        );
     }
-  };
-
-  render() {
-    const useOnlyDarkLogo = this.props.useOnlyDarkLogo;
-
-    return (
-      <nav
-        ref={this.navbar}
-        className={classNames(
-          "navbar navbar-expand-md main-nav navigation fixed-top sidebar-left",
-          { "navbar-expanded": this.state.navbarExpanded }
-        )}
-      >
-        <Container>
-          <button
-            className="navbar-toggler"
-            type="button"
-            onClick={this.toggleNavbar}
-          >
-            <span className="icon-bar" />
-            <span className="icon-bar" />
-            <span className="icon-bar" />
-          </button>
-
-          <NavbarBrand className="mr-lg-5" to="/" tag={Link}>
-            {useOnlyDarkLogo ? (
-              <img
-                alt="..."
-                src={require("assets/img/logo.png")}
-                className="logo logo-sticky"
-              />
-            ) : (
-              <>
-                <img
-                  src={require("assets/img/logo.png")}
-                  alt="Laapp"
-                  className="logo logo-sticky d-block d-md-none"
-                />
-                <img
-                  src={require("assets/img/logo-light.png")}
-                  alt="Laapp"
-                  className="logo d-none d-md-block"
-                />
-              </>
-            )}
-          </NavbarBrand>
-
-          <div className="collapse navbar-collapse">
-            <div className="sidebar-brand">
-              <Link to="/">
-                <img
-                  src={require("assets/img/logo.png")}
-                  alt="Laapp Template"
-                  className="logo"
-                />
-              </Link>
-            </div>
-
-            <Nav className="nav navbar-nav ml-auto" navbar>
-              <NavItem>
-                <NavLink href="#home">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#features">About</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#portfolio">Portfolio</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="http://blog.datumbrain.com" target="_blank">
-                  Blog
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#contact">Contact</NavLink>
-              </NavItem>
-            </Nav>
-          </div>
-        </Container>
-      </nav>
-    );
-  }
 }
 
 export default DefaultNavbar;
